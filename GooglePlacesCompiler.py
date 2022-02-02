2e3a2b527faf6f00b8703378d7b7b198c41e13ce
Jan Kubal <kubaljan@gmail.com>
2022-02-02 10:53:45 +0100

    Working on GooglePlacesCompiler.py

--- GooglePlacesCompiler.py
+++ GooglePlacesCompiler.py
@@ -14,13 +14,21 @@ class GooglePlacesCompiler:
 
     Attributes
     ----------
-    dasda : asdasd
-        Aasdasd.
+    list_of_results : list
+        List containing the results of Places API query. Each member of a list is a dict.
+
+    places_API_df : pd.DataFrame
 
     Methods
     -------
-    adsfsdasd(asdasd):
-        Asdasd.
+    find_first_candidate(name, phone, coordinates, API_KEY)
+        A function that 
+
+    getListOfResults(restaurants_dataframe, API_KEY)
+        A function that applies find_first_candidate on each row of given dataset.
+
+    getDataFrame(list_of_results)
+        A function that concerts list of results into Pandas DataFrame.
 
     """
     def __init__(self, restaurants_dataframe, API_KEY):
@@ -35,6 +43,109 @@ class GooglePlacesCompiler:
         API_KEY : str
             Valid API_KEY generated on Google Cloud Platform (see https://console.cloud.google.com/apis/credentials), with Selected APIs containing Places API (see Key restrictions section).
         """
-        self.restaurants_dataframe = restaurants_dataframe
-        self.MY_API_KEY = API_KEY
+        self.list_of_results = self.getListOfResults(restaurants_dataframe, API_KEY)
+        self.places_API_df = self.getDataFrame(self.list_of_results)
 
+    def find_first_candidate(name, phone, coordinates, API_KEY):
+        '''
+        This function will:
+        1) take dictionary of phones of restaurant and takes the first one and dict of coordinates and saves them
+        2) uses Find place to get first candidates ID
+        3) use this ID to get Details
+        4) return details as a dictionary 
+        
+        The function expects argument phone to be in form {'hlavní telefon': '+420 222 311 221', ...}
+        '''
+        phone_list = []
+        for i in phone:
+            phone_list.append(phone[i])
+        
+        main_phone = phone_list[0] #extracting the main phone from dict
+
+        if coordinates == None:
+            result = {'ZS_name':name, 'name':None, 'formatted_address':None, 'location':None, 'rating':None, 'user_ratings_total':None, 'formatted_phone_number':None}
+        else:
+            latitude = coordinates['latitude']
+            longitude = coordinates['longitude']
+
+        place_request = requests.get(f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={main_phone}&inputtype=textquery&locationbias=circle:50@{latitude},{longitude}&fields=place_id&key={API_KEY}")
+        place_dict = json.loads(place_request.text)
+
+        if place_dict["candidates"] == []:
+            result = {'ZS_name':name, 'name':None, 'formatted_address':None, 'location':None, 'rating':None, 'user_ratings_total':None, 'formatted_phone_number':None}
+        else:
+            iter_id = place_dict["candidates"][0]["place_id"]
+            detail_request = requests.get(f"https://maps.googleapis.com/maps/api/place/details/json?place_id={iter_id}&fields=name%2Cformatted_address%2Cgeometry%2Crating%2Cformatted_phone_number%2Cuser_ratings_total&key={API_KEY}")
+            detail_dict = json.loads(detail_request.text)
+            
+            #saving the details
+            result = {'ZS_name':name}
+
+            try:
+                result['name'] = detail_dict['result']['name']
+            except KeyError:
+                result['name'] = None
+
+            try:
+                result['formatted_address'] = detail_dict['result']['formatted_address']
+            except KeyError:
+                result['formatted_address'] = None
+
+            try:
+                result['location'] = detail_dict['result']['geometry']['location']
+            except KeyError:
+                result['location'] = None
+
+            try:
+                result['rating'] = detail_dict['result']['rating']
+            except KeyError:
+                result['rating'] = None
+
+            try:
+                result['user_ratings_total'] = detail_dict['result']['user_ratings_total']
+            except KeyError:
+                result['user_ratings_total'] = None
+
+            try:
+                result['formatted_phone_number'] = detail_dict['result']['formatted_phone_number']
+            except KeyError:
+                result['formatted_phone_number'] = None            
+
+        return result
+
+    def getListOfResults(self, restaurants_dataframe, API_KEY):
+        """
+        
+        """
+        zs_restaurants = restaurants_dataframe
+        list_of_results = []
+        for ind in zs_restaurants.index:
+            if zs_restaurants["name"][ind] == "Céleste":
+                celeste_exception = {'ZS_name':"Céleste", 'name':None, 'formatted_address':None, 'location':None, 'rating':None, 'user_ratings_total':None, 'formatted_phone_number':None}
+                list_of_results.append(celeste_exception)
+            else:
+                iter_name = zs_restaurants["name"][ind]
+                iter_phones = ast.literal_eval(zs_restaurants["phones"][ind])
+                iter_coordinates = ast.literal_eval(zs_restaurants["coordinates"][ind])
+
+                iter_result = find_first_candidate(iter_name, iter_phones, iter_coordinates, API_KEY)
+
+                list_of_results.append(iter_result)
+        return list_of_results
+
+    def getDataFrame(self, list_of_results):
+        """
+        A function that concerts list of results into Pandas DataFrame.
+
+        Parameters
+        ----------
+        list_of_results : list
+            List containing the results of Places API query. Each member of a list is a dict.
+
+        Returns
+        -------
+        places_API_df : pd.DataFrame
+            Data set containgn information on the restaurants
+        """
+        places_API_df = pd.DataFrame(list_of_results)
+        return places_API_df
